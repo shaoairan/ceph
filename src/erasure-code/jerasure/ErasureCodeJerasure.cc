33dbb1b2b7e1e88060107d436a2277b96185ac99
Myna V <mynaramana@gmail.com>
2017-09-08 00:00:27 +0530

    Added support for multiple erasures within same group (or y crosssection)

--- src/erasure-code/jerasure/ErasureCodeJerasure.cc
+++ src/erasure-code/jerasure/ErasureCodeJerasure.cc
@@ -764,8 +764,8 @@ int ErasureCodeJerasureCLMSR::minimum_to_repair(const set<int> &want_to_read,
   int lost_node_index = 0;
   int rep_node_index = 0;
 
- 
-  if((available_chunks.size() >= (unsigned)d) && (want_to_read.size() <= (unsigned)k+m-d) ){
+
+  if((available_chunks.size() >= (unsigned)d) ){//&& (want_to_read.size() <= (unsigned)k+m-d) ){
 
     for(set<int>::iterator i=want_to_read.begin();
         i != want_to_read.end(); ++i){
@@ -774,19 +774,17 @@ int ErasureCodeJerasureCLMSR::minimum_to_repair(const set<int> &want_to_read,
         if(j != lost_node_index%q) {
            rep_node_index = (lost_node_index/q)*q+j;//add all the nodes in lost node's y column.
            if(rep_node_index < k){
-             assert(want_to_read.find(rep_node_index) == want_to_read.end());
-             minimum->insert(rep_node_index);
+             if(want_to_read.find(rep_node_index) == want_to_read.end())minimum->insert(rep_node_index);
            }
            else if(rep_node_index >= k+nu){
-             assert(want_to_read.find(rep_node_index-nu) == want_to_read.end());
-             minimum->insert(rep_node_index-nu);
+             if(want_to_read.find(rep_node_index-nu) == want_to_read.end())minimum->insert(rep_node_index-nu);
            }
         }
       }
     }
     if (includes(
-	available_chunks.begin(), available_chunks.end(), minimum->begin(), minimum->end())) {
-      for(set<int>::iterator i = available_chunks.begin(); 
+        available_chunks.begin(), available_chunks.end(), minimum->begin(), minimum->end())) {
+      for(set<int>::iterator i = available_chunks.begin();
           i != available_chunks.end(); ++i){
         if(minimum->size() < (unsigned)d){
           if(minimum->find(*i) == minimum->end())minimum->insert(*i);
@@ -796,7 +794,7 @@ int ErasureCodeJerasureCLMSR::minimum_to_repair(const set<int> &want_to_read,
       dout(0) << "minimum_to_repair: shouldn't have come here" << dendl;
       assert(0);
     }
-    
+
   } else{
     dout(0) << "available_chunks: " << available_chunks << " want_to_read:" <<  want_to_read << dendl;
     assert(0);
@@ -865,44 +863,58 @@ void ErasureCodeJerasureCLMSR::group_repair_subchunks(map<int,int> &repair_subch
 
 int ErasureCodeJerasureCLMSR::is_repair(const set<int> &want_to_read,
                                    const set<int> &available_chunks){
-  int lost_node_ind = 0;
-  int rep_node_ind = 0;
 
+  //dout(10)<<__func__<< "want_to_read:" << want_to_read<<"available"<<available_chunks<<dendl;
   if(includes(
-	available_chunks.begin(), available_chunks.end(), want_to_read.begin(), want_to_read.end()) ) return 0;
+        available_chunks.begin(), available_chunks.end(), want_to_read.begin(), want_to_read.end()) ) return 0;
 
-  double repair_fraction = 1.0 - pow(1.0 - (1.0/(double)q), (double)want_to_read.size());
-  if ( ( ((double)d-1.0)*repair_fraction) > ((double)k-1.0)){
-    //decode is better in comparison with repair in this case.
-    return 0;
-  } 
-
-
+  if((d-1)*get_repair_sub_chunk_count(want_to_read) >= (k-1)*sub_chunk_no) return 0;
   if(available_chunks.size() < (unsigned)d) return 0;
-  else if(want_to_read.size() > (unsigned) (k+m-d))return 0;
-  else {
-    for(set<int>::iterator i=want_to_read.begin(); 
-        i != want_to_read.end(); ++i){
-      if(available_chunks.find(*i) != available_chunks.end()) return 0;//if lost index is available.
-      lost_node_ind = (*i < k) ? (*i) : (*i+nu);
-      for(int j=0; j<q; j++){
-        if( j != lost_node_ind%q ) {
-          rep_node_ind = (lost_node_ind/q)*q+j;
-          if(rep_node_ind < k){
-            if(available_chunks.find(rep_node_ind) == available_chunks.end()) return 0;
-          }
-          else if((rep_node_ind >=k) && (rep_node_ind <k+nu))continue;
-          else if(available_chunks.find(rep_node_ind-nu) == available_chunks.end()) return 0;
-        }
+  //else if(want_to_read.size() > (unsigned) (k+m-d))return 0;
+  //in every plane the number of erasures in B can't exceed m
+
+  int erasures_weight_vector[t];
+  int min_y = q+1;
+  memset(erasures_weight_vector, 0, t*sizeof(int));
+
+  for(set<int>::iterator i=want_to_read.begin();
+      i != want_to_read.end(); ++i){
+    int lost_node_id = (*i < k) ? *i: *i+nu;
+    erasures_weight_vector[lost_node_id/q]++;
+    for(int x=0; x < q; x++){
+      int node = (lost_node_id/q)*q+x;
+      node = (node < k) ? node : node-nu;
+      if(want_to_read.find(node) == want_to_read.end()){//node from same group not erased
+        if(available_chunks.find(node) == available_chunks.end()) return 0;//node from same group not available as well
       }
     }
   }
-  return 1;
+  for(int y = 0; y < t; y++){
+    if((erasures_weight_vector[y] > 0) && (erasures_weight_vector[y] < min_y)) min_y = erasures_weight_vector[y];
+  }
+  assert((min_y > 0) && (min_y != (q+1) ));
+
+  if((q+(int)want_to_read.size()-min_y) <= m) return 1;
+  return 0;
 }
 
 int ErasureCodeJerasureCLMSR::get_repair_sub_chunk_count(const set<int> &want_to_read)
 {
-  return sub_chunk_no - pow_int(q-1, want_to_read.size()) * pow_int(q, t - want_to_read.size());
+  int repair_subchunks_count = 1;
+
+  int weight_vector[t];
+  memset(weight_vector, 0, t*sizeof(int));
+
+  for(set<int>::iterator i = want_to_read.begin();
+      i != want_to_read.end(); ++i){
+    weight_vector[(*i)/q]++;
+  }
+
+  for(int y = 0; y < t; y++) repair_subchunks_count = repair_subchunks_count*(q-weight_vector[y]);
+
+  dout(20) << __func__ << " number of repair subchunks:" << sub_chunk_no - repair_subchunks_count << " for repair of want_to_read:"<< want_to_read <<dendl;
+  //return sub_chunk_no - pow_int(q-1, want_to_read.size()) * pow_int(q, t - want_to_read.size());
+  return sub_chunk_no - repair_subchunks_count;
 }
 
 //for any d <=n-1
@@ -923,6 +935,8 @@ int ErasureCodeJerasureCLMSR::repair(const set<int> &want_to_read,
 
 
   map<int, int> repair_sub_chunks_ind;
+
+  //dout(10) << __func__ << " want_to_read:"<<want_to_read<<" sub_chunk_count:"<<repair_sub_chunk_no<< " list of indices list of indices::"<<repair_sub_chunks_ind<<" size of sub chunk list:"<< repair_sub_chunks_ind.size()<<dendl;
   get_repair_subchunks(want_to_read, want_to_read, 0, repair_sub_chunks_ind);
   assert(repair_sub_chunks_ind.size() == (unsigned)repair_sub_chunk_no); 
 
@@ -990,10 +1004,7 @@ int ErasureCodeJerasureCLMSR::repair(const set<int> &want_to_read,
 int ErasureCodeJerasureCLMSR::repair_lost_chunks(map<int,char*> &repaired_data, set<int> &aloof_nodes,
                            map<int, char*> &helper_data, int repair_blocksize, map<int,int> &repair_sub_chunks_ind)
 {
-
-
-
-  unsigned sub_chunksize = repair_blocksize/repair_sub_chunks_ind.size();
+ unsigned sub_chunksize = repair_blocksize/repair_sub_chunks_ind.size();
 
   int z_vec[t];
   map<int, set<int> > ordered_planes;
@@ -1003,6 +1014,8 @@ int ErasureCodeJerasureCLMSR::repair_lost_chunks(map<int,char*> &repaired_data, 
   char *A1, *A2, *B1, *B2;
   int count_retrieved_sub_chunks = 0;
   int num_erased = 0;
+
+  //dout(10) << " lost_nodes " << repaired_data << " aloof_nodes " << aloof_nodes << " helper nodes " << helper_data << "repair_blockssize" << repair_blocksize<< dendl;
 
   for(map<int,int>::iterator i = repair_sub_chunks_ind.begin();
       i != repair_sub_chunks_ind.end(); ++i){
@@ -1024,16 +1037,16 @@ int ErasureCodeJerasureCLMSR::repair_lost_chunks(map<int,char*> &repaired_data, 
 
   //going to use the global B_buf
   assert(B_buf);
-
   for(int i = 0; i < q*t; i++){
     if(B_buf[i]==NULL) B_buf[i] = talloc(char, sub_chunksize*sub_chunk_no);
     assert(B_buf[i]);
-  } 
+  }
 
-  //repair planes in order.
+  //repair planes in order
   for(order=1; ;order++){
     if(ordered_planes.find(order) == ordered_planes.end())break;
     else{
+      //dout(10) << "decoding planes of order " << order <<dendl;
 
       plane_count += ordered_planes[order].size();
       for(set<int>::iterator z=ordered_planes[order].begin();
@@ -1044,25 +1057,28 @@ int ErasureCodeJerasureCLMSR::repair_lost_chunks(map<int,char*> &repaired_data, 
         num_erased = 0;
         for(y=0; y < t; y++){
           for(x = 0; x < q; x++){
-          
+
             node_xy = y*q + x;
-            
-            if( (repaired_data.find(node_xy) != repaired_data.end()) || 
-                (aloof_nodes.find(node_xy) != aloof_nodes.end()) ) 
+
+            if( (repaired_data.find(node_xy) != repaired_data.end()) ||
+                (aloof_nodes.find(node_xy) != aloof_nodes.end()) )
             {//case of erasure, aloof node can't get a B.
              erasure_locations[num_erased] = node_xy;
+             //dout(10)<< num_erased<< "'th erasure of node " << node_xy << " = (" << x << "," << y << ")" << dendl;
              num_erased++;
             } else{//should be in helper data
               assert(helper_data.find(node_xy) != helper_data.end());
               //so A1 is available, need to check if A2 is available.
               A1 = &helper_data[node_xy][repair_plane_to_ind[*z]*sub_chunksize];
 
-              z_sw = (*z) + (x - z_vec[y])*pow_int(q,t-1-y);
+	      z_sw = (*z) + (x - z_vec[y])*pow_int(q,t-1-y);
               node_sw = y*q + z_vec[y];
+              //dout(10) << "current node=" << node_xy << " plane="<< *z << " node_sw=" << node_sw << " plane_sw="<< z_sw << dendl;
               //consider this as an erasure, if A2 not found.
               if(repair_plane_to_ind.find(z_sw) == repair_plane_to_ind.end())
               {
                 erasure_locations[num_erased] = node_xy;
+                //dout(10)<< num_erased<< "'th erasure of node " << node_xy << " = (" << x << "," << y << ")" << dendl;
                 num_erased++;
               } else {
                 if(repaired_data.find(node_sw) != repaired_data.end()){
@@ -1075,72 +1091,97 @@ int ErasureCodeJerasureCLMSR::repair_lost_chunks(map<int,char*> &repaired_data, 
                 } else{
 
                   assert(helper_data.find(node_sw) != helper_data.end());
+                  //dout(10) << "obtaining B1 from A1 A2 for node: " << node_xy << " on plane:" << *z << dendl;
                   A2 = &helper_data[node_sw][repair_plane_to_ind[z_sw]*sub_chunksize];
                   if( z_vec[y] != x){
                     get_B1_fromA1A2(&B_buf[node_xy][repair_plane_to_ind[*z]*sub_chunksize], A1, A2, sub_chunksize);
                   } else memcpy(&B_buf[node_xy][repair_plane_to_ind[*z]*sub_chunksize], A1, sub_chunksize);
                 }
-              }             
-   
+              }
+
             }
 
-            
+
           }//y
         }//x
         erasure_locations[num_erased] = -1;
+        //int erasuresxy[num_erased];
+        //get_erasure_coordinates(erasure_locations, erasuresxy, num_erased);
         //we obtained all the needed B's
         assert(num_erased <= m);
+        //dout(10) << "going to decode for B's in repair plane "<< *z << " at index " << repair_plane_to_ind[*z] << dendl;
         jerasure_matrix_decode_substripe(k+nu, m, w, matrix, 0, erasure_locations, &B_buf[0], &B_buf[k+nu], repair_plane_to_ind[*z], sub_chunksize);
-        
-        
-        for(int i = 0; i < num_erased; i++){
+    for(int i = 0; i < num_erased; i++){
           x = erasure_locations[i]%q;
           y = erasure_locations[i]/q;
+          //dout(10) << "B symbol recovered at (x,y) = (" << x <<","<<y<<")"<<dendl;
+          //dout(10) << "erasure location " << erasure_locations[i] << dendl;
           node_sw = y*q+z_vec[y];
           z_sw = (*z) + (x - z_vec[y]) * pow_int(q,t-1-y);
-          
+
           B1 = &B_buf[erasure_locations[i]][repair_plane_to_ind[*z]*sub_chunksize];
 
           //make sure it is not an aloof node before you retrieve repaired_data
           if( aloof_nodes.find(erasure_locations[i]) == aloof_nodes.end()){
-            if(x == z_vec[y] ){//hole-dot pair
+
+            if(x == z_vec[y] ){//hole-dot pair (type 0)
+              //dout(10) << "recovering the hole dot pair/lost node in repair plane" << dendl;
               A1 = &repaired_data[erasure_locations[i]][*z*sub_chunksize];
               memcpy(A1, B1, sub_chunksize);
               count_retrieved_sub_chunks++;
-            } else {
+            }//can recover next case (type 2) only after obtaining B's for all the planes with same order
+            else {
 
               if(repaired_data.find(erasure_locations[i]) != repaired_data.end() ){//this is a hole (lost node)
-                //A2 for this particular node is available
-                assert(helper_data.find(node_sw) != helper_data.end());
-                assert(repair_plane_to_ind.find(z_sw) !=  repair_plane_to_ind.end());
                 A1 = &repaired_data[erasure_locations[i]][*z*sub_chunksize];
-                A2 = &helper_data[node_sw][repair_plane_to_ind[z_sw]*sub_chunksize];
-                get_type1_A(A1, B1, A2, sub_chunksize);
-                count_retrieved_sub_chunks++;
+                //check if type-2
+                if( repaired_data.find(node_sw) != repaired_data.end()){
+                  if(x < z_vec[y]){//recover both A1 and A2 here
+                    A2 = &repaired_data[node_sw][z_sw*sub_chunksize];
+                    B2 = &B_buf[node_sw][repair_plane_to_ind[z_sw]*sub_chunksize];
+                    gamma_inverse_transform(A1, A2, B1, B2, sub_chunksize);
+                    count_retrieved_sub_chunks = count_retrieved_sub_chunks + 2;
+                  }
+                } else{
+
+                  //dout(10) << "repaired_data" << repaired_data << dendl;
+                  //A2 for this particular node is available
+                  assert(helper_data.find(node_sw) != helper_data.end());
+                  assert(repair_plane_to_ind.find(z_sw) !=  repair_plane_to_ind.end());
+
+                  A2 = &helper_data[node_sw][repair_plane_to_ind[z_sw]*sub_chunksize];
+                  get_type1_A(A1, B1, A2, sub_chunksize);
+                  count_retrieved_sub_chunks++;
+                }
               } else {//not a hole and has an erasure in the y-crossection.
-                
                 assert(repaired_data.find(node_sw) != repaired_data.end());
                 if(repair_plane_to_ind.find(z_sw) == repair_plane_to_ind.end()){
-                  //got to recover A2, if z_sw was already there
-                  dout(10) << "recovering A2 of node:" << node_sw << " at location " << z_sw << dendl;
+                  //i got to recover A2, if z_sw was already there
+                  //dout(10) << "recovering A2 of node:" << node_sw << " at location " << z_sw << dendl;
                   A1 = &helper_data[erasure_locations[i]][repair_plane_to_ind[*z]*sub_chunksize];
                   A2 = &repaired_data[node_sw][z_sw*sub_chunksize];
                   get_type2_A(A2, B1, A1, sub_chunksize);
-                  count_retrieved_sub_chunks++;                
+                  count_retrieved_sub_chunks++;
                 }
               }
 
-            }
-          }
-        }
-       
+            }//type-1 erasure recovered.
+          }//not an aloof node
+        }//erasures
 
-      }
+       //dout(10) << "repaired data after decoding at plane: " << *z << " "<< repaired_data << dendl;
+       //dout(10) << "helper data after decoding at plane: " << *z << " "<< helper_data << dendl;
+
+      }//planes of a particular order
+
     }
   }
   assert(repair_sub_chunks_ind.size() == (unsigned)plane_count);
   assert(sub_chunk_no*repaired_data.size() == (unsigned)count_retrieved_sub_chunks);
 
+  //dout(10) << "repaired_data = " << repaired_data << dendl;
+
+  //for(int i=0; i<q*t ; i++)free(B_buf[i]);
   return 0;
 }
 
